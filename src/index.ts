d7b73ed1c33c763b0ff590c5fc05d3be364eeccd
joe molina <joedodaniljr123@gmail.com>
2024-12-23 14:35:55 -0400

    configurtacion sin cors

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,32 @@ import products from "./api/products/products.routes";
 import cookieParser from "cookie-parser";
 import Orders from "./api/orders/orders.routes";
 import path from 'path';
-import { CorsOptions } from "cors";
+// import { CorsOptions } from "cors";
 
-const allowedOrigins: string[] = [
-  "http://localhost:3001",
-  "https://pedidos-client.vercel.app",
-  "https://pedidos-client-dni37cv5t-joemolinas-projects.vercel.app"
-];
+// const allowedOrigins: string[] = [
+//   "http://localhost:3001",
+//   "https://pedidos-client.vercel.app",
+//   "https://pedidos-client-dni37cv5t-joemolinas-projects.vercel.app"
+// ];
 
-const corsOptions: CorsOptions = {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Permitir acceso
-    } else {
-      callback(new Error("No autorizado por CORS")); // Rechazar acceso
-      console.log('diablo pero q paso')
-    }
-  },
-  credentials: true, // Permitir el envío de cookies
-};
+// const corsOptions: CorsOptions = {
+//   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true); // Permitir acceso
+//     } else {
+//       callback(new Error("No autorizado por CORS")); // Rechazar acceso
+//       console.log('diablo pero q paso')
+//     }
+//   },
+//   credentials: true, // Permitir el envío de cookies
+// };
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/auth", auth);
