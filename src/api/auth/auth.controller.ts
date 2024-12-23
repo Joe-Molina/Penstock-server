e5202ce06292e71b7e32fef8ee4013f538d7d287
joe molina <joedodaniljr123@gmail.com>
2024-12-23 14:45:44 -0400

    token update time expire

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -142,7 +142,7 @@ export class Auth {
           },
           JWT_SECRET_KEY,
           {
-            expiresIn: "1000000000S",
+            expiresIn: "7d",
           }
         );
         const matchPassword = bcrypt.compareSync(password, user.password);
