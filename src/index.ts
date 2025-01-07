6e30533a361d7433edb819057caa8787edfc0ed9
joe molina <joedodaniljr123@gmail.com>
2025-01-07 11:12:41 -0400

    modificaciones al cors

--- src/index.ts
+++ src/index.ts
@@ -5,45 +5,43 @@ import products from "./api/products/products.routes";
 import cookieParser from "cookie-parser";
 import Orders from "./api/orders/orders.routes";
 import path from 'path';
-import session from 'express-session';
-import { Request, Response, NextFunction } from 'express';
+// import session from 'express-session';
+// import { Request, Response, NextFunction } from 'express';
 
-declare module 'express-session' {
-  interface Session {
-    user?: { username: string, email: string, role: string, lodef: boolean, id: number };
-  }
-}
+// declare module 'express-session' {
+//   interface Session {
+//     user?: { username: string, email: string, role: string, lodef: boolean, id: number };
+//   }
+// }
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
   origin: ["http://localhost:3001", "https://pedidos-client.vercel.app"], // Dominio del cliente
-  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
-  allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
   credentials: true, // Permitir el envío de cookies
 }));
+app.use(cookieParser());
 
-app.use(
-  session({
-    secret: 'el_mejor_secreto_del_mundo_mundiall', // Cambia esto por una cadena más segura en producción
-    resave: false, // Evita que la sesión se guarde si no hay cambios
-    saveUninitialized: false, // No guarda sesiones vacías
-    cookie: {
-      secure: false, // Cambia a true si usas HTTPS
-      maxAge: 24 * 60 * 60 * 1000, // 1 día en milisegundos
-    },
-  })
-);
+// app.use(
+//   session({
+//     secret: 'el_mejor_secreto_del_mundo_mundiall', // Cambia esto por una cadena más segura en producción
+//     resave: false, // Evita que la sesión se guarde si no hay cambios
+//     saveUninitialized: false, // No guarda sesiones vacías
+//     cookie: {
+//       secure: false, // Cambia a true si usas HTTPS
+//       maxAge: 24 * 60 * 60 * 1000, // 1 día en milisegundos
+//     },
+//   })
+// );
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  if (req.session.user) {
-    next();
-  } else {
-    res.status(401).json({ message: 'No estás autenticado' });
-  }
-};
+// export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//   if (req.session.user) {
+//     next();
+//   } else {
+//     res.status(401).json({ message: 'No estás autenticado' });
+//   }
+// };
 
 app.use("/auth", auth);
 app.use("/products", products);
@@ -60,6 +58,7 @@ app.get('/pruebacookie', (_req, res) => {
     maxAge: 60 * 60 * 24 * 7, // La cookie expirará en 1 semana
     path: '/',
     sameSite: 'none'               // La cookie es válida para todas las rutas
+
   });
 
   // Enviar una respuesta al cliente
