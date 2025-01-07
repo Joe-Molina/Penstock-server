1ad7257b006dd80c3159fe079f4c768a1a133247
joe molina <joedodaniljr123@gmail.com>
2025-01-07 11:16:44 -0400

    q sirva el session y despues lo quito xd

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ import products from "./api/products/products.routes";
 import cookieParser from "cookie-parser";
 import Orders from "./api/orders/orders.routes";
 import path from 'path';
-// import session from 'express-session';
-// import { Request, Response, NextFunction } from 'express';
+import session from 'express-session';
+import { Request, Response, NextFunction } from 'express';
 
-// declare module 'express-session' {
-//   interface Session {
-//     user?: { username: string, email: string, role: string, lodef: boolean, id: number };
-//   }
-// }
+declare module 'express-session' {
+  interface Session {
+    user?: { username: string, email: string, role: string, lodef: boolean, id: number };
+  }
+}
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
@@ -23,25 +23,25 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
-// app.use(
-//   session({
-//     secret: 'el_mejor_secreto_del_mundo_mundiall', // Cambia esto por una cadena más segura en producción
-//     resave: false, // Evita que la sesión se guarde si no hay cambios
-//     saveUninitialized: false, // No guarda sesiones vacías
-//     cookie: {
-//       secure: false, // Cambia a true si usas HTTPS
-//       maxAge: 24 * 60 * 60 * 1000, // 1 día en milisegundos
-//     },
-//   })
-// );
+app.use(
+  session({
+    secret: 'el_mejor_secreto_del_mundo_mundiall', // Cambia esto por una cadena más segura en producción
+    resave: false, // Evita que la sesión se guarde si no hay cambios
+    saveUninitialized: false, // No guarda sesiones vacías
+    cookie: {
+      secure: false, // Cambia a true si usas HTTPS
+      maxAge: 24 * 60 * 60 * 1000, // 1 día en milisegundos
+    },
+  })
+);
 
-// export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-//   if (req.session.user) {
-//     next();
-//   } else {
-//     res.status(401).json({ message: 'No estás autenticado' });
-//   }
-// };
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ message: 'No estás autenticado' });
+  }
+};
 
 app.use("/auth", auth);
 app.use("/products", products);
